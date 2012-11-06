8b1062b0f860f4fe2ce70ed24fe677948c1c0978
BlueForeverI <bluef0rever@abv.bg>
2012-11-06 16:44:19 +0200

    The editing functionality of the control is completed. TODO: add functions for loading/savind *.docx files

--- DocumentEditorTestApp/DocumentEditor.xaml.cs
+++ DocumentEditorTestApp/DocumentEditor.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -24,81 +25,6 @@ namespace DocumentEditorTestApp
             InitializeComponent();
         }
 
-        private void btnPaste_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnCopy_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnCut_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnUndo_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnRedo_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnBold_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnItalic_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnUnderline_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnLeftAlign_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnCenterAlign_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnRightAlign_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnJustifyAlign_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnBullets_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnNumbering_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
             this.comboFontFamily.ItemsSource = System.Windows.Media.Fonts.SystemFontFamilies;
@@ -110,6 +36,108 @@ namespace DocumentEditorTestApp
                 32.0, 34.0, 36.0, 38.0, 40.0, 44.0, 48.0, 52.0, 56.0, 60.0, 64.0, 68.0, 72.0, 76.0,
                 80.0, 88.0, 96.0, 104.0, 112.0, 120.0, 128.0, 136.0, 144.0
             };
+        }
+
+        private void rtbDocument_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateItemCheckedState(btnBold, TextElement.FontWeightProperty, FontWeights.Bold);
+            UpdateItemCheckedState(btnItalic, TextElement.FontStyleProperty, FontStyles.Italic);
+            UpdateItemCheckedState(btnUnderline, Inline.TextDecorationsProperty, TextDecorations.Underline);
+
+            UpdateItemCheckedState(btnLeftAlign, Paragraph.TextAlignmentProperty, TextAlignment.Left);
+            UpdateItemCheckedState(btnRightAlign, Paragraph.TextAlignmentProperty, TextAlignment.Right);
+            UpdateItemCheckedState(btnCenterAlign, Paragraph.TextAlignmentProperty, TextAlignment.Center);
+            UpdateItemCheckedState(btnJustifyAlign, Paragraph.TextAlignmentProperty, TextAlignment.Justify);
+
+            UpdateSelectionListType();
+            UpdateSelectedFontFamily();
+            UpdateSelectedFontSize();
+            UpdateForegroundColor();
+        }
+
+        private void UpdateItemCheckedState(ToggleButton button, DependencyProperty formattingProperty, object expectedValue)
+        {
+            object currentValue = rtbDocument.Selection.GetPropertyValue(formattingProperty);
+            button.IsChecked = (currentValue == DependencyProperty.UnsetValue) ? false : currentValue != null && currentValue.Equals(expectedValue);
+        }
+
+        private void UpdateSelectionListType()
+        {
+            Paragraph startParagraph = rtbDocument.Selection.Start.Paragraph;
+            Paragraph endParagraph = rtbDocument.Selection.End.Paragraph;
+
+            if(startParagraph != null && endParagraph != null 
+                && (startParagraph.Parent is ListItem) 
+                && (endParagraph.Parent is ListItem) 
+                && object.ReferenceEquals(((ListItem)startParagraph.Parent).List, ((ListItem)endParagraph.Parent).List))
+            {
+                TextMarkerStyle markerStyle = ((ListItem) startParagraph.Parent).List.MarkerStyle;
+                if(markerStyle == TextMarkerStyle.Disc)
+                {
+                    btnBullets.IsChecked = true;
+                }
+                else if(markerStyle == TextMarkerStyle.Decimal)
+                {
+                    btnNumbering.IsChecked = true;
+                }
+            }
+            else
+            {
+                btnBullets.IsChecked = false;
+                btnNumbering.IsChecked = false;
+            }
+        }
+
+        private void comboFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FontFamily fontFamily = (FontFamily) e.AddedItems[0];
+            ApplyPropertyToText(TextElement.FontFamilyProperty, fontFamily);
+        }
+
+        private void comboFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ApplyPropertyToText(TextElement.FontSizeProperty, e.AddedItems[0]);
+        }
+
+        private void ApplyPropertyToText(DependencyProperty property, object value)
+        {
+            if(value == null)
+            {
+                return;
+            }
+
+            rtbDocument.Selection.ApplyPropertyValue(property, value);
+        }
+
+        private void UpdateSelectedFontFamily()
+        {
+            object value = rtbDocument.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+            FontFamily currentFontFamily = (FontFamily) ((value == DependencyProperty.UnsetValue) ? null : value);
+            if(currentFontFamily != null)
+            {
+                comboFontFamily.SelectedItem = currentFontFamily;
+            }
+        }
+
+        private void UpdateSelectedFontSize()
+        {
+            object value = rtbDocument.Selection.GetPropertyValue(TextElement.FontSizeProperty);
+            comboFontSize.SelectedValue = (value == DependencyProperty.UnsetValue) ? null : value;
+        }
+
+        private void colorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color> e)
+        {
+            ApplyPropertyToText(TextElement.ForegroundProperty, new SolidColorBrush(colorPicker.SelectedColor));
+        }
+
+        private void UpdateForegroundColor()
+        {
+            object value = rtbDocument.Selection.GetPropertyValue(TextElement.ForegroundProperty);
+            SolidColorBrush colorBrush = (SolidColorBrush) ((value == DependencyProperty.UnsetValue) ? null : value);
+            if(colorBrush != null)
+            {
+                colorPicker.SelectedColor = colorBrush.Color;
+            }
         }
     }
 }
